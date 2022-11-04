222944f747045fd0993a9d120c65e2dee1984c90
Jaideep2912 <jaideep2912@gmail.com>
2022-11-05 00:35:18 +0530

    Community Page Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Collaboration from './components/Collaboration';
+import Community from './components/Community';
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Community />
       <Collaboration />
     </div>
   )
